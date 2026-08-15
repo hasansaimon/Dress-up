581d87e3c05b04a62a57dc4ecfa4d2bfd05381a2
Hasan Saimon <296305728+hasansaimon@users.noreply.github.com>
2026-08-15 14:54:55 +0600

    Add MediaPipe and OpenCV dependencies
    Added MediaPipe and OpenCV libraries to dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -118,6 +118,8 @@ dependencies {
   implementation(libs.okhttp)
   // implementation(libs.play.services.location)
   implementation(libs.retrofit)
+implementation(libs.mediapipe.tasks.vision)
+implementation(libs.opencv)
   testImplementation(libs.androidx.compose.ui.test.junit4)
   testImplementation(libs.androidx.core)
   testImplementation(libs.androidx.junit)
@@ -136,6 +138,4 @@ dependencies {
   debugImplementation(libs.androidx.compose.ui.tooling)
   "ksp"(libs.androidx.room.compiler)
   "ksp"(libs.moshi.kotlin.codegen)
-implementation(libs.mediapipe.tasks.vision)
-implementation(libs.opencv)
 }
